8b4f90858c20a9c0065137fda80e5e005af83952
Hugo1991 <hugofernandezvisier@gmail.com>
2016-05-16 16:07:20 +0200

    fileDownloaderCompleto

--- src/pc2/FileDownloader.java
+++ src/pc2/FileDownloader.java
@@ -14,7 +14,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Semaphore;
 import java.util.regex.Pattern;
 
@@ -22,32 +25,31 @@ public class FileDownloader {
 	private static final String RUTADESCARGA = "downloads";
 	private String fichero;
 	private int numMaxDescargas;
-	private static CountDownLatch latchMerge;
-	private static CountDownLatch latchDescarga;
+	private CyclicBarrier merge;
 	private static Semaphore semMaxDescargas;
+	private static CountDownLatch latchDescarga;
 
 	public FileDownloader(int numMaxDescargas) {
 		super();
 		this.numMaxDescargas = numMaxDescargas;
-		latchDescarga = new CountDownLatch(1);
-		semMaxDescargas = new Semaphore(numMaxDescargas);
+		semMaxDescargas = new Semaphore(this.numMaxDescargas);
 	}
 
-	public void startDownload() throws InterruptedException {
+	@SuppressWarnings("resource")
+	public void startDownload() {
 		try {
 			FileReader f = new FileReader(fichero);
 			BufferedReader b = new BufferedReader(f);
 			String linea = b.readLine();
 			
 			while (!(linea == null)) {
-				if (latchDescarga.getCount() != 1)
-					latchDescarga.await();
 				String[] datos = linea.split("\\s+");
 				String ruta = datos[0];
 				String nombre = datos[1];
 				int numPartes = Integer.parseInt(datos[2]);
-				latchMerge = new CountDownLatch(numPartes);
-				String num = "";
+				latchDescarga = new CountDownLatch(numPartes);
+				String num = null;
+				merge = new CyclicBarrier(numPartes, new MergeFile(RUTADESCARGA, nombre));
 				for (int i = 0; i < numPartes; i++) {
 					if (i < 10)
 						num = "0" + i;
@@ -55,61 +57,89 @@ public class FileDownloader {
 						num = String.valueOf(i);
 					String rutaOrigen = ruta + "/" + nombre + ".part" + i;
 					String rutaDestino = RUTADESCARGA + "/" + nombre + ".part" + num;
-					new Thread(new Runnable() {
-						public void run() {
-							try {
-								downloadFile(rutaOrigen, rutaDestino);
 
-							} catch (InterruptedException e) {
-								e.printStackTrace();
-							}
-						}
-					}, "hilo" + i).start();
+					new DownloadFile(rutaOrigen, rutaDestino, merge);
 
 				}
-				latchMerge.await();
-				mergeFile(RUTADESCARGA, nombre);
-				deleteFiles(RUTADESCARGA, nombre);
+				
+				latchDescarga.await();
+				
 				linea = b.readLine();
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
-		}
-	}
-
-	private void downloadFile(String url, String path) throws InterruptedException {
-		semMaxDescargas.acquire();
-		try {
-			URL website = new URL(url);
-			File folder = new File(path.split("/")[0]);
-			if (!folder.exists())
-				folder.mkdir();
-			InputStream in = website.openStream();
-			Path pathOut = Paths.get(path);
-			Files.copy(in, pathOut, StandardCopyOption.REPLACE_EXISTING);
-			in.close();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		semMaxDescargas.release();
-		latchMerge.countDown();
 	}
 
 	/**
-	 * Metodo encargado de unir las partes que forman los ficheros de un
-	 * directorio
+	 * Metodo encargado de eliminar los ficheros, hace un recorrido sobre el
+	 * directorio buscando los archivos que se llamen con el nombre del fichero
 	 * 
 	 * @param dir
-	 *            nombre del directorio
+	 *            directorio de los ficheros
 	 * @param fileStart
-	 *            nombre del fichero a unir
+	 *            nombre del fichero a borrar
 	 */
-	private void mergeFile(String dir, String fileStart) {
-		latchDescarga.countDown();
+	static void deleteFiles(String dir, String fileStart) {
+
+		for (String fileName : dameListaFicheros(dir, fileStart))
+			new File(dir + "/" + fileName).delete();
+	}
+
+	/**
+	 * Funcion encargada de devolver un listado de ficheros de un directorio y
+	 * sobre un nombre de fichero
+	 * 
+	 * @param dir
+	 *            directorio del fichero
+	 * @param fileStart
+	 *            nombre del fichero
+	 * @return array de nombre de ficheros
+	 */
+	static String[] dameListaFicheros(String dir, String fileStart) {
+
+		String[] files = new File(dir)
+				.list((path, name) -> Pattern.matches(fileStart + Pattern.quote(".") + "part.*", name));
+		Arrays.sort(files);
+		return files;
+	}
+
+	public void setFichero(String fichero) {
+		this.fichero = fichero;
+	}
+
+	public static Semaphore getSemMaxDescargas() {
+		return semMaxDescargas;
+	}
+
+	public static CountDownLatch getLatchDescarga() {
+		return latchDescarga;
+	}
+}
+
+/**
+ * Metodo encargado de unir las partes que forman los ficheros de un directorio
+ * 
+ * @param dir
+ *            nombre del directorio
+ * @param fileStart
+ *            nombre del fichero a unir
+ */
+
+class MergeFile implements Runnable {
+	String fichero = null;
+	String dir = null;
+
+	public MergeFile(String dir, String fileStart) {
+		fichero = fileStart;
+		this.dir = dir;
+	}
+
+	public void mergeFile(String dir, String fileStart) {
 		FileInputStream fis = null;
 		FileOutputStream fos = null;
 		try {
@@ -118,7 +148,7 @@ public class FileDownloader {
 				ofile.delete();
 			int bytesRead = 0;
 			fos = new FileOutputStream(ofile, true);
-			for (String fileName : dameListaFicheros(dir, fileStart)) {
+			for (String fileName : FileDownloader.dameListaFicheros(dir, fileStart)) {
 				File f = new File(dir + "/" + fileName);
 				fis = new FileInputStream(f);
 				byte[] fileBytes = new byte[(int) f.length()];
@@ -138,46 +168,65 @@ public class FileDownloader {
 			fis = null;
 			fos = null;
 		}
+
+	}
+
+	@Override
+	public void run() {
+
+		System.out.println("Descargado fichero "+fichero);
+		mergeFile(dir, fichero);
+		FileDownloader.deleteFiles(dir, fichero);
 		
+
+	}
+}
+
+class DownloadFile implements Runnable {
+	String url = null;
+	String path = null;
+	CyclicBarrier merge = null;
+
+	public DownloadFile(String url, String path, CyclicBarrier merge) {
+		this.url = url;
+		this.path = path;
+		this.merge = merge;
+		new Thread(this).start();
 	}
 
-	/**
-	 * Metodo encargado de eliminar los ficheros, hace un recorrido sobre el
-	 * directorio buscando los archivos que se llamen con el nombre del fichero
-	 * 
-	 * @param dir
-	 *            directorio de los ficheros
-	 * @param fileStart
-	 *            nombre del fichero a borrar
-	 */
-	private void deleteFiles(String dir, String fileStart) {
-		for (String fileName : dameListaFicheros(dir, fileStart))
-			new File(dir + "/" + fileName).delete();
-	}
+	@Override
+	public void run() {
+		downloadFile(url, path);
+		FileDownloader.getLatchDescarga().countDown();
 
-	/**
-	 * Funcion encargada de devolver un listado de ficheros de un directorio y
-	 * sobre un nombre de fichero
-	 * 
-	 * @param dir
-	 *            directorio del fichero
-	 * @param fileStart
-	 *            nombre del fichero
-	 * @return array de nombre de ficheros
-	 */
-	private String[] dameListaFicheros(String dir, String fileStart) {
-		return new File(dir).list((path, name) -> Pattern.matches(fileStart + Pattern.quote(".") + "part.*", name));
-	}
-
-	public void setFichero(String fichero) {
-		this.fichero = fichero;
-	}
-
-	public static void main(String[] args) {
-		FileDownloader fichero = new FileDownloader(3);
-		fichero.setFichero("entrada.txt");
 		try {
-			fichero.startDownload();
+			merge.await();
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		} catch (BrokenBarrierException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+	private void downloadFile(String url, String path) {
+		try {
+			FileDownloader.getSemMaxDescargas().acquire();
+			URL website = new URL(url);
+			File folder = new File(path.split("/")[0]);
+			if (!folder.exists())
+				folder.mkdir();
+			InputStream in = website.openStream();
+			Path pathOut = Paths.get(path);
+			Files.copy(in, pathOut, StandardCopyOption.REPLACE_EXISTING);
+			in.close();
+			FileDownloader.getSemMaxDescargas().release();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
--- src/pc2/principal.java
+++ src/pc2/principal.java
@@ -1,5 +1,11 @@
 package pc2;
 
 public class principal{
-	
+	public static void main(String[] args) {
+		FileDownloader fichero = new FileDownloader(2);
+		fichero.setFichero("entrada.txt");
+
+		fichero.startDownload();
+
+	}
 }
