0badee3301e6f67ed82edd10d5c339f7dad6ebc9
Hugo1991 <hugofernandezvisier@gmail.com>
2016-05-16 17:33:01 +0200

    funciona

--- src/pc2/principal.java
+++ src/pc2/principal.java
@@ -3,7 +3,7 @@ package pc2;
 public class principal {
 	public static void main(String[] args) {
 		FileDownloader fichero = new FileDownloader(2);
-		fichero.setFichero("entrada.txt");
+		fichero.setFichero("resources/entrada.txt");
 		fichero.startDownload();
 
 	}
