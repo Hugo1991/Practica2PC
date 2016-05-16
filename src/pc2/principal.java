66cce82185c8033396f4394af2c5aa16cac2b68d
Hugo1991 <hugofernandezvisier@gmail.com>
2016-05-16 17:43:10 +0200

    finish

--- src/pc2/principal.java
+++ src/pc2/principal.java
@@ -1,9 +1,16 @@
 package pc2;
 
+import java.util.Scanner;
+
 public class principal {
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		FileDownloader fichero = new FileDownloader(2);
-		fichero.setFichero("resources/entrada.txt");
+		System.out.println("Introduzca el fichero de entrada");
+		String ficheroEntrada = new Scanner(System.in).nextLine();
+		System.out.println("Introduzca el numero de descargas maximo");
+		int numMaxDescargas=new Scanner(System.in).nextInt();
+		FileDownloader fichero = new FileDownloader(numMaxDescargas);
+		fichero.setFichero(ficheroEntrada);
 		fichero.startDownload();
 
 	}
